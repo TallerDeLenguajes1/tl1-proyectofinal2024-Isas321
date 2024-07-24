94e8959d0fdf1942ec2ae4ad28eee45161f50069
Isas321 <javier.isas321@gmail.com>
2024-07-24 10:37:26 -0300

    Creacion de EspacioJuegoDeTronos

---
+++ manejoApi.cs
@@ -0,0 +1,26 @@
+// Root myDeserializedClass = JsonSerializer.Deserialize<List<Root>>(myJsonResponse);
+
+using System.Text.Json.Serialization;
+
+namespace EspacioJuegoDeTronos{
+  public class Miembros
+  {
+      [JsonPropertyName("name")]
+      public string  nombreCompleto { get; set; }
+
+      [JsonPropertyName("slug")]
+      public string  primerNombre { get; set; }
+  }
+
+  public class Root
+  {
+      [JsonPropertyName("slug")]
+      public string  apellido { get; set; }
+
+      [JsonPropertyName("name")]
+      public string  nombreDeCasa { get; set; }
+
+      [JsonPropertyName("members")]
+      public List<Miembros>  miembros { get; set; }
+  }
+}
