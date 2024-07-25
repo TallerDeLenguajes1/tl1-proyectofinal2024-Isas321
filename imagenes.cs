486a1a964c3b7cd567cf9f332a8598da07589f3e
Isas321 <javier.isas321@gmail.com>
2024-07-24 21:02:32 -0300

    nueva imagen de inicio y con colores

--- imagenes.cs
+++ imagenes.cs
@@ -2,58 +2,49 @@ namespace EspacioImagenes;
 
 class Imagenes(){
   public static void caballero(){
-
-      string[] asciiArt = new string[]
-        {
-            "",
-            "",
-            "                                         .:kkdc. ",                                                                                 
-            "                               .;ldxkOOkxdxKMMWXd.",                                                                             
-            "                            'ckXWMMMMMMMMMMMMMMMW0d; .'",                                                                            
-            "                .'.     .'ckXMMMMMMMMMMMMMMMMMMMMMMO:d0,",                                                                         
-            "               .dNKxolox0NMMMMMMMMMMMMMMMMMMMMMMMMMWNWK,",                                                                   
-            "               .kMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXo::cloooolc:,.",                                                                   
-            "                oWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNNNNNWWWXOdc'",                                                                   
-            "                .xWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXd;'...',:lxKNWNOc.   .",                                                                   
-            "                 .c0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMK:           .,lONWKdlkO:",                                                 
-            "                   .,cdKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWo               .:0WMMMMX:",                                            
-            "                     .lKMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNl              .;oKWW0kXMX:",                                        
-            "                   .cOWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMKl.        .'cxKWWXk:. cNM0,",                                     
-            "        .... ...,cxKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOkddoodk0NWWKxc.    .xWWk.",                              
-            "        'kK000KNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKxl;.        ;KMNc ",                            
-            "         ;0WMMMMMMMMMMMMMMMMMMMMMMMMMMWNNMMMMMMMMMMMMMMMMMWNKkdoc;,;::ccloodddo;'kMMx.",                           
-            "          .l0WMMMMMMMMMMMMMMMMMMMMMMMWx,dWMMMMMMMMMMMMMMMMWXOl.   .xWMMMMMMMMMMXclNM0'",                          
-            "            .,lk0XNWWWNNX00NMMMMMMMMWx. ;XMMMMMMMMMMMMMMMMMMMNl    'OWMMMMMMMMMM0xXMX;",                          
-            "                ..'',,'.. ,0MMMMMMMXo.  .OMMMMMMMMMMMMMMMMMMMWo     .ckXWMMMMMMMMWWMWkc.",                         
-            "                          ;XMMMMWKd,     lWMMMMMMMMMMMMMMMMMMO'    .'. .,:loxk0KXXNWMMNl",                        
-            "                          l00Oxo:.       .kMMMMMMMMMMMMMMMMMX:     'ko  .,'   ....'lXMX:",                       
-            "                           ..            .kWMMMMMMMMMMMMMMMM0'    .d0;  ,0O.  ,xl. cNMO.",                       
-            "                                        :0WWNWMMMMMMMMMMMMMMNo.  .oKl  'kKc   lKl .kMWo ",                      
-            "                                     .,xNWKlcKMMMMMMWWMMMMMMMW0l'.;;. ;0No.  cKx..oNM0'",                      
-            "                             .;codkkO0NMMNko0WMMMMWO:;lONWMMMMMMNOl' .:xc. .dXk. cXMX: ",                      
-            "                         .,oOXWMMMMMMMMMMMMMMMMMMMWKo;..,lkXWMMMMMMNOo:.  .lOo. cXMXc",                      
-            "                       .cONMMMMMMMMMMMMMMMMMMMMMMMMMMWX0xoodkKWMMMMMMMWXkl;'. .oNMK:",                     
-            "                     .:0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWMMWNNWMMMMMMMMMMMWXOx0WWO,",                   
-            "                    .xNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNKOkOKNWMMMNXNWMMMWXKNWKo.",                  
-            "                   .kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXkdl:cOMMk'':lll;..',.",                  
-            "                  .xWMMMMMMMWNXXKKKKKXXNWWMMMMMMMMMMMMMMMMMMMWK0XMMx.",                  
-            "                  cNWNKOxoc:,'.........',;coxOKNMMMMMMMMMMMMMMMMMMMO' ",                   
-            "                 .;c;'.                       .':oxkKWMMMMMMMMMMMMMNl",                   
-            "                                                    .,lx0NWMMMMMMMMM0,",                   
-            "                                                        ..;coxkkkkxxo' ",
-            "",
-            "",  
-
-        };
-
-        // Mostrar el arte ASCII en la consola
-        Console.ForegroundColor = ConsoleColor.White;
-        foreach (string line in asciiArt)
-        {
-            Console.WriteLine(line);
-        }
-        Console.ResetColor();
-      
+        Console.Clear();
+        PrintColoredText("", ConsoleColor.White, "", ConsoleColor.White);
+        PrintColoredText("", ConsoleColor.White, "", ConsoleColor.White);
+        PrintColoredText("                                'cc;.                                         ", ConsoleColor.Yellow, "", ConsoleColor.White);
+        PrintColoredText("                      .,codxxdoxKMWXd'                                    ", ConsoleColor.Yellow, ".,,,'... .',,.                                                     ", ConsoleColor.Cyan);
+        PrintColoredText("                   .:xKWMMMMMMMMMMMMM0; '.                              ", ConsoleColor.Yellow, ",d0NWWNKOxxONWW0:                                                    ", ConsoleColor.Cyan);
+        PrintColoredText("         .;:.. .'cxXWMMMMMMMMMMMMMMMMM0x0l                             ", ConsoleColor.Yellow, ".:'',:cld0Kd;:oo;                                                     ", ConsoleColor.Cyan);
+        PrintColoredText("         ,KWX00KNWMMMMMMMMMMMMMMMMMMMMMMNl.........                           ", ConsoleColor.Yellow, "'OMX;                                                         ", ConsoleColor.Cyan);
+        PrintColoredText("         '0MMMMMMMMMMMMMMMMMMMMMMMMMMMMMWX00KXNXXX0Oxl;.                      ", ConsoleColor.Yellow, "cNMK,           .;ddc.                                         ", ConsoleColor.Cyan);
+        PrintColoredText("          cXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMXxc:;::ldOXNKx;.                   ", ConsoleColor.Yellow, "oWM0'         'x000NWXk;                                       ", ConsoleColor.Cyan);
+        PrintColoredText("           ;kXWMMMMMMMMMMMMMMMMMMMMMMMMMMMK:         .;dKNOodk:              ", ConsoleColor.Yellow, ".dMM0'         oWNc.;0MWo                                       ", ConsoleColor.Cyan);
+        PrintColoredText("             .:kNMMMMMMMMMMMMMMMMMMMMMMMMWo             ,OWMMMXc             ", ConsoleColor.Yellow, ".xMM0'        .dWN:  oWNc                                       ", ConsoleColor.Cyan);
+        PrintColoredText("             .c0WMMMMMMMMMMMMMMMMMMMMMMMMWx.         .,lONXklxNX:            ", ConsoleColor.Yellow, ".xMM0'        .dMX:  oWNc                                       ", ConsoleColor.Cyan);
+        PrintColoredText("          .;o0WMMMMMMMMMMMMMMMMMMMMMMMMMMMW0oc:;,;:lxKNNOl'  .kM0'           ", ConsoleColor.Yellow, ".xMWkcc:;,'',..xMNl..dWWo.                                      ", ConsoleColor.Cyan);
+        PrintColoredText("   'oxddxOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWX0xc'      :XWd.          ", ConsoleColor.Yellow, ".OWWWMMMWWN0d'.dNWNkokNMKc.                                     ", ConsoleColor.Cyan);
+        PrintColoredText("   .cXMMMMMMMMMMMMMMMMMMMMMMWNWMMMMMMMMMMMMMWXOxoc:cccclloooo:;OM0'          ", ConsoleColor.Yellow, ".,;;:clllc;.    ':c,..,:,                                      ", ConsoleColor.Cyan);
+        PrintColoredText("     'o0WMMMMMMMMMMMMMMMMMMNo;OMMMMMMMMMMMMMWXx'   cXMMMMMMMMNodWX:                                                                         ", ConsoleColor.Yellow, "", ConsoleColor.Cyan);
+        PrintColoredText("       .,cdxkOkkxoxNMMMMMMXl. oWMMMMMMMMMMMMMMWd.  .lKWMMMMMMMXKWWo.                                                                        ", ConsoleColor.Yellow, "", ConsoleColor.Cyan);
+        PrintColoredText("                  ;XMMMMNO;   ;XMMMMMMMMMMMMMMNc    .':ok0KXNWWMMMXk,                                                                       ", ConsoleColor.Yellow, "", ConsoleColor.Cyan);
+        PrintColoredText("                  lKXKko,.    .dWMMMMMMMMMMMMWd.   .l;  .'..,;:cl0MK;                                                                       ", ConsoleColor.Yellow, "", ConsoleColor.Cyan);
+        PrintColoredText("                  ....         oNMMMMMMMMMMMMNc    ck, .xx. .c: .kMO.        ", ConsoleColor.Yellow, "'lxkkxdolc:,.                                                  ", ConsoleColor.Cyan);
+        PrintColoredText("                             .dNNXWMMMMMMMMMMWk'  ;Ol .o0c  ;Ol.cXNl       ", ConsoleColor.Yellow, ".:::cok0K0kOKk'                              ..                   ", ConsoleColor.Cyan);
+        PrintColoredText("                       ..'';oKW0ldXMMMN0KWMMMMMXx:;,..dKl  ;Od.,0WO.       ", ConsoleColor.Yellow, "'.     ...cXWd                              ;k:                   ", ConsoleColor.Cyan);
+        PrintColoredText("                  .'cdOKXNNWMMMXKNMMMWk,.;d0WMMMMWKx:,;, .l0d.'OW0;                 ", ConsoleColor.Yellow, ".OMWo   .:;.   ,:.      .,ll;.....dNWk:'     ':,.        ", ConsoleColor.Cyan);
+        PrintColoredText("                .lONMMMMMMMMMMMMMMMMMMWKkocldOXWMMMMWXOo;';,.,OW0,                  ", ConsoleColor.Yellow, ",0MWl  ,kWWd. lNWO'   .ldox0Oxo:.;0MW0o;. ,lkKWN0o,.     ", ConsoleColor.Cyan);
+        PrintColoredText("              .lKWMMMMMMMMMMMMMMMMMMMMMMMMWWNXXWMMMMMMMMWXOdxXWk'                   ", ConsoleColor.Yellow, ",KMNc  .dWNl  :XMx.  .xWKl'...   .dWWl   ,KM0:cOWM0,     ", ConsoleColor.Cyan);
+        PrintColoredText("             'kWMMMMMMMMMMMMMMMMMMMMMMMMMMNKOOOKNWMNKKWWWN00X0l.                    ", ConsoleColor.Yellow, ",KMNc   lWNl  ;XMx.  .dWMWXOl;.  .xMWl   ;XMk. ,KMk.     ", ConsoleColor.Cyan);
+        PrintColoredText("            ,0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXOdldXMO'.,;;'....                      ", ConsoleColor.Yellow, ";KMX;   lWNl  ;XMx.   .;okKWMWKl..xMNl   ;XMx. '0Mk.     ", ConsoleColor.Cyan);
+        PrintColoredText("           .kMMWNX0Oxdoooooodxk0KNWMMMMMMMMMMMMWNWM0'                     ", ConsoleColor.Yellow, ".:ll:'.   ;XWk.   oWNl  cXMx.      ..;o0WK,.xMNc   :XMk. ,KMO.     ", ConsoleColor.Cyan);
+        PrintColoredText("           ,xdl;'..             .';ldOXWMMMMMMMMMMMX:                   ", ConsoleColor.Yellow, ".lkKNMWNKkdoOXx.   .xWW0ooOWMK: .;dkOOxoco0x.'OWW0c..cNMXxcdXMNx'    ", ConsoleColor.Cyan);
+        PrintColoredText("                                      .;okKNMMMMMMMMO.                  ", ConsoleColor.Yellow, "''..':oxOOOko,      .cxd;..lxo' ':;,;cdkko,   .cxd;. .:dkxc,:dx:.    ", ConsoleColor.Cyan);
+        PrintColoredText("                                          .,:ldxxxxdl.                                                                                       ", ConsoleColor.Yellow, "", ConsoleColor.Cyan);
+        PrintColoredText("", ConsoleColor.White, "", ConsoleColor.White);
     }
-  
+
+    static void PrintColoredText(string text1, ConsoleColor color1, string text2, ConsoleColor color2)
+    {
+        ConsoleColor originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = color1;
+        Console.Write(text1);
+        Console.ForegroundColor = color2;
+        Console.Write(text2);
+        Console.ForegroundColor = originalColor;
+        Console.WriteLine();
+    }
 }
