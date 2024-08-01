4c7943ac6e15aa979f51bdf8532257420e6aa191
Isas321 <javier.isas321@gmail.com>
2024-08-01 17:05:26 -0300

    modularizacion y cambio de carreras a enfrentamientos con turnos de combate + cte de ajuste

--- juegoDeCaballeros.cs
+++ juegoDeCaballeros.cs
@@ -1,57 +1,65 @@
 using  EspacioPersonaje;
 namespace EspacioJuegoDeCaballeros;
-
-public class Enfrentamiento{
-  public static Personaje RealizarEnfrentamiento(Personaje caballero1, Personaje caballero2)
-  {
-      int carrera = 0;
-      do
-      {
-        carrera++;
-        Console.WriteLine($"\nCarrera numero [{carrera}]");
-        int danioCaballero1 = CalcularDanio(caballero1, caballero2);
-        caballero2.Salud -= danioCaballero1;
-        if(caballero2.Salud<0){
-          caballero2.Salud=0;
-        }
-        Console.WriteLine($"{caballero1.NombreCompleto} ataca a {caballero2.NombreCompleto} y provoca {danioCaballero1} puntos de daño.");
-        Console.WriteLine($"{caballero2.NombreCompleto} tiene {caballero2.Salud} puntos de salud restantes.");
-
-        if (caballero2.Salud <= 0)
+ public static class Enfrentamiento
+    {
+        public static Personaje RealizarEnfrentamiento(Personaje caballero1, Personaje caballero2)
         {
-            Console.WriteLine($"\n{caballero2.NombreCompleto} ha sido derrotado. ¡{caballero1.NombreCompleto} es el ganador!");
-            return caballero1;
+            int carrera = 1;
+            do
+            {
+                Console.WriteLine($"\nEnfrentamiento [{carrera++}]");
+                Console.WriteLine($"\n* Turno de {caballero1.NombreCompleto}");
+                if (RealizarAtaque(caballero1, caballero2))
+                {
+                    return caballero1;
+                }
+                Console.WriteLine($"\n* Turno de {caballero2.NombreCompleto}");
+                if (RealizarAtaque(caballero2, caballero1))
+                {
+                    return caballero2;
+                }
+            } while (carrera <=3 && caballero1.Salud>0 && caballero2.Salud>0);
+
+            return DeterminarGanador(caballero1, caballero2);
         }
 
-        int danioCaballero2 = CalcularDanio(caballero2, caballero1);
-        caballero1.Salud -= danioCaballero2;
-        if(caballero1.Salud<0){
-          caballero1.Salud=0;
-        }
-        Console.WriteLine($"\n{caballero2.NombreCompleto} ataca a {caballero1.NombreCompleto} y provoca {danioCaballero2} puntos de daño.");
-        Console.WriteLine($"{caballero1.NombreCompleto} tiene {caballero1.Salud} puntos de salud restantes.");
-
-        if (caballero1.Salud <= 0)
+        private static bool RealizarAtaque(Personaje atacante, Personaje defensor)
         {
-            Console.WriteLine($"\n{caballero1.NombreCompleto} ha sido derrotado. ¡{caballero2.NombreCompleto} es el ganador!");
-            return caballero2;
+            int danio = CalcularDanio(atacante, defensor);
+            defensor.Salud -= danio;
+            if (defensor.Salud < 0)
+            {
+                defensor.Salud = 0;
+            }
+
+            Console.WriteLine($"{atacante.NombreCompleto} ataca a {defensor.NombreCompleto} y provoca {danio} puntos de daño.");
+            Console.WriteLine($"{defensor.NombreCompleto} tiene {defensor.Salud} puntos de salud restantes.");
+
+            if (defensor.Salud <= 0)
+            {
+                Console.WriteLine($"\n{defensor.NombreCompleto} ha sido derrotado. ¡{atacante.NombreCompleto} es el ganador!");
+                return true;
+            }
+
+            return false;
         }
 
-      } while (carrera<5);
+        private static Personaje DeterminarGanador(Personaje caballero1, Personaje caballero2)
+        {
+            if (caballero1.Salud > caballero2.Salud)
+            {
+                Console.WriteLine($"\n¡{caballero1.NombreCompleto} es el ganador con más salud restante!");
+                return caballero1;
+            }
+            else if (caballero2.Salud > caballero1.Salud)
+            {
+                Console.WriteLine($"\n¡{caballero2.NombreCompleto} es el ganador con más salud restante!");
+                return caballero2;
+            }
 
-      if (caballero1.Salud > caballero2.Salud)
-      {
-          Console.WriteLine($"\n¡{caballero1.NombreCompleto} es el ganador con más salud restante!");
-          return caballero1;
-      }
-      else if (caballero2.Salud > caballero1.Salud)
-      {
-          Console.WriteLine($"\n¡{caballero2.NombreCompleto} es el ganador con más salud restante!");
-          return caballero2;
-      }
-      Console.WriteLine("\n¡El enfrentamiento termina en empate!");
-      return null;
-  }
+            Console.WriteLine("\n¡El enfrentamiento termina en empate!");
+            return null;
+        }
 
   public static int CalcularDanio(Personaje atacante, Personaje defensor)
   {
@@ -59,18 +67,18 @@ public class Enfrentamiento{
       int ataque = atacante.Destreza * atacante.Fuerza * atacante.Nivel;
       int efectividad = Utilidades.ObtenerIntRandom(1, 101);
       int defensa = defensor.Armadura * defensor.Velocidad;
-      const int constanteDeAjuste = 85;
+      const int constanteDeAjuste = 50;
       int danioProvocado = ((ataque * efectividad) - defensa) / constanteDeAjuste;
 
       if (danioProvocado < 0)
       {
-          danioProvocado = 0; // Asegurar que el daño no sea negativo
+          danioProvocado = 0; 
       }
       return danioProvocado;
   }
+    }
 
 
 
 
-
-}
+// }
