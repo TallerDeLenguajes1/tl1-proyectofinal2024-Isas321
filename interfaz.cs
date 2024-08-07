4a4e2f2cae3f5fd9f2ced70bf21f23bd26364737
Isas321 <javier.isas321@gmail.com>
2024-08-07 19:24:29 -0300

    se cambia descripcion del juego

--- interfaz.cs
+++ interfaz.cs
@@ -4,7 +4,7 @@ public class Interfaz{
   public static int Menu(){
     int op;
     do{
-      Console.WriteLine("\t\t\t\t\t========    MENU    ========\n");
+      Console.WriteLine("\n\t\t\t\t\t=========    MENU    =========\n");
       Console.WriteLine("\t\t\t\t\t 1- Lista de caballeros");
       Console.WriteLine("\t\t\t\t\t 2- Participar en el torneo");
       Console.WriteLine("\t\t\t\t\t 3- Eliminar y cargar personajes");
@@ -47,17 +47,16 @@ public class Interfaz{
     }
 
     public static void Presentacion(){
+      Console.WriteLine("\n");
+      Console.WriteLine($@"
+  ¡Bienvenido al torneo de justas de caballeros de Juego de Tronos!
+  Enfréntate a los más valientes guerreros, demuestra tu destreza y avanza en el torneo.
+  Solo los mejores prevalecerán. ¡Prepárate para la gloria y que el mejor caballero triunfe!
+  El honor y la valentía serán puestos a prueba. ¿Estás listo para hacer historia?
+  ");
+      Console.Write("\n  Presione una tecla para comenzar... ");
+      Console.ReadKey();
+      Console.Clear();
       Console.WriteLine("\n\n");
-        Console.WriteLine("¡Bienvenido a 'Apuestas de Juego de Tronos'!");
-        Console.WriteLine("Un emocionante juego de apuestas.");
-        Console.WriteLine("Elige a uno de los personajes de Juego de Tronos.");
-        Console.WriteLine("Apuesta por su victoria en Justas de caballeros.");
-        Console.WriteLine("Analiza bien sus fortalezas y ten en cuenta que cualquiera puede tener un mal dia.");
-        Console.WriteLine("¡Que comience la diversión y la intriga!");
-        Console.WriteLine("\n\n");
-        Console.Write("Presione una tecla para comenzar... ");
-        Console.ReadKey();
-        Console.Clear();
-        Console.WriteLine("\n\n");
     }
 }
